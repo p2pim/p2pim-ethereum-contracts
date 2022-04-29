3e542d00f415300e0030b7f7b7dd1d04568da147
Fernando Rincon <f.rincon@protonmail.com>
2022-04-29 12:53:54 +0800

    feat: add openzeppeline to the rust library

--- rust/lib.rs
+++ rust/lib.rs
@@ -1,11 +1,19 @@
-use ethcontract::contract;
 
-contract!(
+ethcontract::contract!(
     pub "build/contracts/P2pimMasterRecord.json",
     mod = master_record,
 );
 
-contract!(
+ethcontract::contract!(
     pub "build/contracts/P2pimAdjudicator.json",
     mod = adjudicator,
 );
+
+pub mod third {
+    pub mod openzeppelin {
+        ethcontract::contract!(
+            pub "node_modules/@openzeppelin/contracts/build/contracts/IERC20Metadata.json",
+            mod = ierc20_metadata
+        );
+    }
+}
